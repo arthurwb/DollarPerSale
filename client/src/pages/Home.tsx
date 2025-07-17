c781e07daa32ebaf47602a0f0808ccf51f179c04
Brooks <brooksarthur07@gmail.com>
2025-07-16 22:07:02 -0400

    uhhhhh

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+
+function Home() {
+  return (
+    <div>
+        this is the text on the home page iu
+    </div>
+  );
+}
+
+export default Home;
---
+++ client/src/pages/Layout.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+
+function Layout() {
+  return (
+    <div className='layout'>
+        <div id='scrolling text' className='p-2'>
+            SCROLLING TEXT HERE
+        </div>
+        <header className='p-2 mb-2 flex h-35'>
+            <div className='flex items-center me-2'>
+                <img id='logo' src='200x200.png' className='border-2 p-1 h-full object-cover'></img>
+            </div>
+            <div className='w-full'>
+                <div className='bg-black flex flex-row'>
+                    <img src='800x100.png' className='border-2 p-1 w-full max-h-20 object-cover'></img>
+                </div>
+                <div className='flex flex-row pt-4 justify-between underline nav_items'>
+                    <a href='/'>About $PS</a>
+                    <a href='/'>$PZ Archive</a>
+                    <a href='/'>Contributors</a>
+                    <a href='/'>Support</a>
+                    <a href='/'>Contact</a>
+                </div>
+            </div>
+        </header>
+        <div className='flex flex-row'>
+            <main id='mainContent' className='lg:basis-5/6 w-full border-1 lg:me-2 p-1 max-lg:mx-auto'>
+                <Outlet />
+            </main>
+            <div id='sidebar' className='relative basis-1/6 border-1 min-h-[75dvh] -top-12'>
+                <div className='border-1 p-1 m-1'>
+                    <h1>test</h1>
+                    testing the text to see if it all fits all the time
+                </div>
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default Layout;
